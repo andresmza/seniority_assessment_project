cda08ec50742faf208744c04e3aba611dc26e08b
Andrés Ortega <andres.mza25@gmail.com>
2022-10-14 09:35:37 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    
+    Route::get('/subjects', function () {
+        return view('subjects');
+    })->name('subjects');
+
+    Route::get('/teachers', function () {
+        return view('teachers');
+    })->name('teachers');
+    
+    Route::get('/students', function () {
+        return view('students');
+    })->name('students');
+
+
+
+
+});
